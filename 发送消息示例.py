bfd64098212b075302b9bc1af6f8677a3c96e00a
Scipline <Scipline52@gmail.com>
2023-12-24 10:27:49 +0800

    update

--- 发送消息示例.py
+++ 发送消息示例.py
@@ -1,3 +1,12 @@
+'''
+@Author       : Scipline
+@Since        : 2023-12-24 10:02:46
+@LastEditor   : Scipline
+@LastEditTime : 2023-12-24 10:27:10
+@FileName     : 发送消息示例.py
+@Description  : 
+new Env('发送消息示例');
+'''
 #!usr/bin/env python3
 from notify import send
 
